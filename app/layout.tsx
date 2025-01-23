404199025a850aafe833348ca9c61e7b58d325a1
André de Faria <adfariacarvalho@gmail.com>
2025-01-22 22:52:54 -0300

    feat: Pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,30 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+  }) {
   return (
     <html lang="pt-br">
+      <head>
+        {/* Script para carregar o Pixel do Facebook */}
+        <script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1612459173032421');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
