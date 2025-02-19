9eb22ecf6f65f9622777efd315133f3d63fb4446
André de Faria <adfariacarvalho@gmail.com>
2025-02-18 21:57:14 -0300

    feat: add script google

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,23 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
+        {/* Google Analytics Tag */}
+                <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11519677004"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11519677004');
+            `,
+          }}
+        />
         {/* Script para carregar o Pixel do Facebook */}
         <Script
           id="facebook-pixel"
